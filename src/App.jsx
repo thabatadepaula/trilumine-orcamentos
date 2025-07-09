605ee931df0a92b5701a643f2130f5dd1e7c9f07
thabatadepaula <thabatadepaulaoliveira@gmail.com>
2025-07-09 18:10:57 -0300

    burrice

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { Routes, Route, Link } from "react-router-dom";
 import Materiais from "./pages/Materiais";
 import Clientes from "./pages/Clientes";
+import Produtos from "./pages/Produtos"; // caso já tenha
 
-
-function App() {
+export default function App() {
   return (
-    <>
-      <Materiais />
-      <Clientes />
-    </>
+    <div style={{ padding: "2rem" }}>
+      <nav style={{ marginBottom: "2rem" }}>
+        <Link to="/" style={{ marginRight: "1rem" }}>Materiais</Link>
+        <Link to="/clientes" style={{ marginRight: "1rem" }}>Clientes</Link>
+        <Link to="/produtos">Produtos</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Materiais />} />
+        <Route path="/clientes" element={<Clientes />} />
+        <Route path="/produtos" element={<Produtos />} />
+      </Routes>
+    </div>
   );
 }
-
-export default App;
